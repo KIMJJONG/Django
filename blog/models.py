c8484badaaed95d2800f319400455f9bd301ee75
KIMJJONG <kimjjong327@gmail.com>
2017-03-19 11:02:42 +0900

    테스트 커밋

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
 
     class Meta:
         verbose_name = 'post'
-        verbose_name_plural = 'posts'
+        verbose_name_plural = 'posts'dfd
         db_table = 'my_post'
         ordering = ('-modify_date',)
 
